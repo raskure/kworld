6a96b2f7cc5d6b336a91f589b6035f0534e7b865
convexy <108969241+raskure@users.noreply.github.com>
2022-07-18 22:06:29 +0900

    dev

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ window.addEventListener("load", function () {
 
   scene.background = new THREE.Color(0xffffff);
 
+  putGrid(scene);
   const cubes = putWheelOfBoxes(scene);
+  cubes2 = putWheelOfBoxes(scene, num = 100, size = 0.1, radius = 5, color = 0x0000ff);
 
   const alight = new THREE.AmbientLight(0xffffff, 0.6);
   scene.add(alight);
@@ -31,37 +33,48 @@ window.addEventListener("load", function () {
     cube.rotation.x += 0.1 * dt;
     cube.rotation.y += 0.1 * dt;
     rotateWheelOfBoxes(cubes, dt);
+    let wd = camera.getWorldDirection(new THREE.Vector3);
+    let wu = (new THREE.Vector3()).copy(camera.up);
     for (let key in keystate) {
+      let tmp = new THREE.Vector3();
       switch (key) {
         case "w":
-          camera.position.z += -keystate[key] * dt;
+          tmp.copy(wd).multiplyScalar(dt);
+          camera.position.add(tmp);
           break;
         case "s":
-          camera.position.z += keystate[key] * dt;
+          tmp.copy(wd).negate().multiplyScalar(dt);
+          camera.position.add(tmp);
           break;
         case "a":
-          camera.position.x += -keystate[key] * dt;
+          tmp.crossVectors(wd, wu).negate().multiplyScalar(dt);
+          camera.position.add(tmp);
           break;
         case "d":
-          camera.position.x += keystate[key] * dt;
+          tmp.crossVectors(wd, wu).multiplyScalar(dt);
+          camera.position.add(tmp);
           break;
         case "ArrowUp":
-          camera.rotation.x += keystate[key] * dt;
+          tmp.copy(wu).multiplyScalar(dt).add(wd).add(camera.position);
+          camera.lookAt(tmp);
           break;
         case "ArrowDown":
-          camera.rotation.x += -keystate[key] * dt;
+          tmp.copy(wu).negate().multiplyScalar(dt).add(wd).add(camera.position);
+          camera.lookAt(tmp);
           break;
         case "ArrowLeft":
-          camera.rotation.y += keystate[key] * dt;
+          tmp.crossVectors(wd, wu).negate().multiplyScalar(dt).add(wd).add(camera.position);
+          camera.lookAt(tmp);
           break;
         case "ArrowRight":
-          camera.rotation.y += -keystate[key] * dt;
+          tmp.crossVectors(wd, wu).multiplyScalar(dt).add(wd).add(camera.position);
+          camera.lookAt(tmp);
           break;
         case "mousemove.x":
-          camera.rotation.y += keystate[key] * dt;
+          camera.rotation.y += keystate[key] * dt / 30;
           break;
         case "mousemove.y":
-          camera.rotation.x += keystate[key] * dt;
+          camera.rotation.x += keystate[key] * dt / 30;
           break;
       }
     }
@@ -83,8 +96,6 @@ window.addEventListener("load", function () {
   let id = -1;
   window.addEventListener("mousemove", function (e) {
     let d = Math.sqrt(e.movementX * e.movementX + e.movementY * e.movementY);
-    e.preventDefault();
-    console.log(d);
     if (!isNaN(d) && d != 0) {
       keystate["mousemove.x"] = e.movementX;
       keystate["mousemove.y"] = e.movementY;
@@ -103,13 +114,43 @@ window.addEventListener("load", function () {
 });
 
 
+function putGrid(scene) {
+  let num = 10;
+  let color = 0xcccccc;
+  for (let i = -num; i <= num; i++) {
+    for (let j = -num; j <= num; j++) {
+      let line = new THREE.Line(
+        new THREE.BufferGeometry().setFromPoints([
+          new THREE.Vector3(i, j, num),
+          new THREE.Vector3(i, j, -num)
+        ]),
+        new THREE.LineBasicMaterial({ color: color })
+      );
+      scene.add(line);
+      line = new THREE.Line(
+        new THREE.BufferGeometry().setFromPoints([
+          new THREE.Vector3(i, num, j),
+          new THREE.Vector3(i, -num, j)
+        ]),
+        new THREE.LineBasicMaterial({ color: color })
+      );
+      scene.add(line);
+      line = new THREE.Line(
+        new THREE.BufferGeometry().setFromPoints([
+          new THREE.Vector3(num, i, j),
+          new THREE.Vector3(-num, i, j)
+        ]),
+        new THREE.LineBasicMaterial({ color: color })
+      );
+      scene.add(line);
+    }
+  }
+}
 
-
-
-function putWheelOfBoxes(scene, num = 50, size = 0.1, radius = 2) {
+function putWheelOfBoxes(scene, num = 50, size = 0.1, radius = 2, color = 0x00ff00) {
   let cubes = new Array();
   for (let i = 0; i < num; i++) {
-    let cube = new THREE.Mesh(new THREE.BoxGeometry(size, size, size), new THREE.MeshStandardMaterial({ color: 0x00ff00 }));
+    let cube = new THREE.Mesh(new THREE.BoxGeometry(size, size, size), new THREE.MeshStandardMaterial({ color: color }));
     cube.position.x = radius * Math.cos(2 * Math.PI * i / num);
     cube.position.y = radius * Math.sin(2 * Math.PI * i / num);
     cube.rotation.z = 2 * Math.PI * i / num;
